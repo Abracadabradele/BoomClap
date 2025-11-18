d06fd42aa1e3f8218dee28adfd1c6bc236401689
Adele <mcshanealincoln@gmail.com>
2025-11-18 09:20:24 -0500

    Co-authored-by: Dillan <dillan.g.pdx@gmail.com>

--- demo/src/main/java/Main.java
+++ demo/src/main/java/Main.java
@@ -1,5 +1,38 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.control.*;
+import javafx.geometry.Pos;
+
+public class Main extends Application {
   public static void main(String[] args) {
-    System.out.println("Boom Clap!");
+    launch(args);
   }
-}
+  @Override
+    public void start(Stage mainStage) {
+      Color backgroundColor = Color.rgb(131, 156, 201);
+      Color keyColor = Color.rgb(170, 191, 191);
+      Color sharpFlatKey = Color.rgb(5, 19, 46);
+      Pane bgPane = new Pane();
+      bgPane.setPrefWidth(400.0);
+      bgPane.setPrefHeight(400.0);
+      
+      Background backckground = new Background(new BackgroundFill(backgroundColor, null, null));
+      bgPane.setBackground(backckground);
+      
+
+
+      // Label greeting = new Label();
+      // greeting.("hello");
+      
+      VBox vBox = new VBox(bgPane);
+      //vBox.getChildren().addAll(greeting);
+      
+      Scene scene = new Scene(vBox, 400, 400);
+      mainStage.setScene(scene);
+      mainStage.show();
+    } 
+    
+  }
