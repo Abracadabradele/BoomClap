759c6a6539a373a99f5c8950bebba283c0a40328
Dillan <dillan.g.pdx@gmail.com>
2025-11-27 10:35:40 -0500

    Update ButtonTileApp.java
    so mr d said to js do tile pane then work on overlapping when get back...I voided that. stack pane based on what I read seems to have the ability to overlap them. I used for loops for the buttons cus some of the tutorials did, others just made each one. I think for loops may make it easier if u plan on adding a bunch of buttons. idk if this looks how I want it to or even displays cus, yea, no java fx, but I dont think any syntax errors and I think the logic is fine except to the black keys. either it it'll place where I want it to, which is the first black second black no black third black 4th black 5th black, and the empty region all be the rest, or it'll just place in order. prettier made the code look good and I copied that bit where each button prints when it clicks. I dont know if I needed Java.util.scanner tho. oopsies. we still need to make it so the buttons display their keys, move or change color when pressed, and obviously map to a sound. also may need to add hgap

--- demo/src/main/java/ButtonTileApp.java
+++ demo/src/main/java/ButtonTileApp.java
@@ -5,33 +5,45 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
 
 public class ButtonTileApp extends Application {
-
     @Override
     public void start(Stage primaryStage) {
-        // Create a TilePane
-        TilePane tilePane = new TilePane();
-        tilePane.setPadding(new Insets(20, 10, 20, 10)); // Add padding
-        tilePane.setHgap(10); // Horizontal gap between tiles
-        tilePane.setVgap(10); // Vertical gap between tiles
-        tilePane.setAlignment(Pos.CENTER); // Center the tiles in the pane
 
-        // Create several buttons and add them to the TilePane
-        for (int i = 1; i <= 9; i++) {
-            Button button = new Button("Button " + i);
-            button.setPrefSize(100, 100); // Set preferred size for uniform look
-            // Optional: Make buttons expand to fill the tile size
-            button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-            button.setOnAction(e -> System.out.println(button.getText() + " clicked!"));
-            tilePane.getChildren().add(button);
+        TilePane whiteKeys = new TilePane();
+        whiteKeys.setPrefColumns(7);
+        whiteKeys.setHgap(1);
+        whiteKeys.setAlignment(Pos.CENTER);
+        for (int i = 0; i < 7; i++) {
+            Button wb = new Button();
+            wb.setPrefSize(50, 100);
+            wb.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #000000;");
+            wb.setOnAction(e -> System.out.println(" clicked!"));
+            whiteKeys.getChildren().add(wb);
         }
 
-        // Create the scene and show the stage
-        Scene scene = new Scene(tilePane, 400, 400);
-        primaryStage.setTitle("Button Tile Plane (JavaFX)");
+        TilePane blackKeys = new TilePane();
+        blackKeys.setPrefColumns(7);
+        blackKeys.setHgap(5);
+        blackKeys.setAlignment(Pos.TOP_CENTER);
+        for (int i = 0; i < 7; i++) {
+            if (i == 0 || i == 1 || i == 3 || i == 4 || i == 5) {
+                Button bb = new Button();
+                bb.setPrefSize(25, 60);
+                bb.setStyle("-fx-background-color: #000000;");
+                bb.setOnAction(e -> System.out.println(" clicked!"));
+                blackKeys.getChildren().add(bb);
+            } else
+                blackKeys.getChildren().add(new javafx.scene.layout.Region());
+        }
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(whiteKeys, blackKeys);
+        Scene scene = new Scene(stackPane, 500, 150);
+        primaryStage.setTitle("Piano");
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
