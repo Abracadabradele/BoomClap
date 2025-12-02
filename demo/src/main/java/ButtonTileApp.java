a6432c4a01b5f8447450a81af6b8c3fe3f946bc0
Dillan <dillan.g.pdx@gmail.com>
2025-12-01 23:40:18 -0500

    Update ButtonTileApp.java

--- demo/src/main/java/ButtonTileApp.java
+++ demo/src/main/java/ButtonTileApp.java
@@ -1,53 +1,52 @@
 import java.io.File;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.TilePane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.*;
+import javafx.scene.web.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.Region;
 
 public class ButtonTileApp extends Application {
     private MediaPlayer player;
     @Override
     public void start(Stage primaryStage) {
-
-        TilePane whiteKeys = new TilePane();
-        whiteKeys.setPrefColumns(7);
-        whiteKeys.setHgap(1);
-        whiteKeys.setAlignment(Pos.TOP_CENTER);
+        Pane root = new Pane();
+     
+        
         for (int i = 0; i < 7; i++) {
             Button wb = new Button(""+(i+1));
             wb.setPrefSize(50, 100);
+            wb.setLayoutX(i*50);
+            wb.setLayouyY(0);
             wb.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #000000;");
             wb.setOnAction(e -> {
-                    int buttonNum = Integer.parseInt(wb.getText());
-                    int octaveNum =4;
-                    playNote currentNote = new playNote(octaveNum);
-                    String fullpath = currentNote.getFullPathWB(buttonNum);
-                    Media media = new Media(new File(fullpath).toURI().toString());
-                    player = new MediaPlayer(media);
-                    player.setVolume(1.0); //set volume
-                    player.play();
-                    System.out.println("note plated: " + fullpath);
-                    
-                });
-            whiteKeys.getChildren().add(wb);
-        }
+            int buttonNum = Integer.parseInt(wb.getText());
+            int octaveNum = 4;
+            playNote currentNote = new playNote(octaveNum);
+            String fullpath = currentNote.getFullPathWB(buttonNum);
+            Media media = new Media(new File(fullpath).toURI().toString());
+            player = new MediaPlayer(media);
+            player.setVolume(1.0); // set volume
+            player.play();
+            System.out.println("note plated: " + fullpath);
 
-        TilePane blackKeys = new TilePane();
-        blackKeys.setPrefColumns(7);
-        blackKeys.setHgap(17);
-        blackKeys.setAlignment(Pos.TOP_CENTER);
+        });
+        root.getChildren().add(wb);
+        }
+        
         for (int i = 0; i < 7; i++) {
             if (i == 0 || i == 1 || i == 4 || i == 5 || i == 6) {
                 Button bb = new Button(""+(i+1));
                 bb.setPrefSize(25, 60);
+                bb.setLayoutY(0);
+                bb.setLayoutX(37.5*(i+1));
                 bb.setStyle("-fx-background-color: #000000;");
                 bb.setOnAction(e -> {
                     int buttonNum = Integer.parseInt(bb.getText());
@@ -61,19 +60,14 @@ public class ButtonTileApp extends Application {
                     System.out.println("note plated: " + fullpath);
         
                 });
-                blackKeys.getChildren().add(bb);
+                root.getChildren().add(bb);
             } else{
-                Region transparentRegion = new Region();
-                transparentRegion.setMouseTransparent(true);
-                blackKeys.getChildren().add(transparentRegion);
+               continue;
             }
         }
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(whiteKeys, blackKeys);
-        Scene scene = new Scene(stackPane, 500, 150);
-        primaryStage.setTitle("Piano");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    Scene scene = new Scene(root,350,100);
+    primaryStage.setTitle("Dillan is so cool and smart!🤤😍-Adele");
+    primaryStage.show();
 
     }
 
